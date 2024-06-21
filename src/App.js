888a6cbace720c639da4bced767094d1f0b3a4dc
MayankCh0udhary <mayank07choudhary@gmail.com>
2024-06-21 08:14:08 +0530

    Dark mode - Commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
@@ -10,7 +11,8 @@ function App() {
       <Navbar title='TextUtils' aboutText='About'/>
       {/* <Navbar/> */}
       
-      <TextForm/>
+      {/* <TextForm/> */}
+      <About/>
     </>
   );
 }
